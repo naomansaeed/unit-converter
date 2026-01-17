93cafb83556854ecd60ece787cb700ceecae5182
Naoman Saeed <naoman.saeed@gmail.com>
2026-01-17 08:47:44 +0500

    feat: added code to load the volume module.

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
     // 3. Add category-specific content
     if (category === 'length') {
         contentHTML = `
-            <input type="number" id="input-value" placeholder="Enter value">
+            <input type="number" id="input-value" placeholder="Enter value (e.g., inches)">
             <div class="conversion-result">
                 <div>
                     <span id="imperial-value">0.000</span> <span id="imperial-unit">in</span>
@@ -40,7 +40,7 @@
     } 
     else if (category === 'mass') {
         contentHTML = `
-            <input type="number" id="input-value" placeholder="Enter value">
+            <input type="number" id="input-value" placeholder="Enter value (e.g., pounds)">
             <div class="conversion-result">
                 <div>
                     <span id="imperial-value">0.000</span> <span id="imperial-unit">lb</span>
@@ -54,11 +54,32 @@
         `;
     }
 
+    else if (category === 'volume') {
+        contentHTML = `
+            <input type="number" id="input-value" placeholder="Enter value (e.g., gallons)">
+            <div class="conversion-result">
+                <div>
+                    <span id="imperial-value">0.000</span>
+                    <span id="imperial-unit">gallons</span>
+                </div>
+            <div class="separator">↔</div>
+                <div>
+                    <span id="metric-value">0.000</span>
+                    <span id="metric-unit">liters</span>
+                </div>
+            </div>
+            <button id="convert-btn">Convert to Metric</button>
+        `;
+    }
+
     // For now, just show the category name
-    moduleView.innerHTML = `<h2>${title}</h2>
-                            ${contentHTML} <br>
-                            <p></p>
-                            <button id="back-btn">← Back</button>    
+    moduleView.innerHTML = `<div id="loop-back">
+                                <button id="back-btn">← Back</button>
+                            </div>
+                            <h2>${title}</h2>
+                            
+                            ${contentHTML} 
+                                
                             `;
 
     // Back button listener
